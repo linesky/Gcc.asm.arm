81b147ee25d01eb739bff5d8bb51e8da162859ff
sky line <32512284+linesky@users.noreply.github.com>
2024-10-23 19:38:11 +0100

    Add files via upload

--- fill.c
+++ fill.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-extern int _fill(int *a,char b,int c);
+extern int _fills(char *a,char b,int c);
 //arm-linux-gnueabihf-gcc -o len len.c len.o -static
 
 int main(){
     char cc[1024];
-    _fill(cc,'\0',240);
-    _fill(cc,'*',200);
+    _fills(cc,'\0',240);
+    _fills(cc,'*',200);
     printf("%s.....\n",cc);
     return 0;
 
