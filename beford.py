a9d8c4519540f81be55eacb93fa9022f6e779c69
sky line <32512284+linesky@users.noreply.github.com>
2024-11-01 18:03:08 +0000

    Add files via upload

---
+++ beford.py
@@ -0,0 +1,17 @@
+tabs=4
+codes=""
+files=input("file to convert to tab ford?")
+f1=open(files,"r")
+codes=f1.read()
+f1.close()
+codess=codes.split("\n")
+codes=""
+
+for n in codess:
+    if codes.find(" "*tabs)!=0:
+        codes=codes + n+"\n"
+    else:
+        codes=codes + n[4:]+"\n"
+f1=open(files+".out","w")
+f1.write(codes)
+f1.close()
